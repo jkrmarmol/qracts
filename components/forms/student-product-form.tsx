ca1858a8df419cc60ee363287acb5b1a7e067164
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2024-07-12 00:01:30 +0800

    added create student api

--- components/forms/student-product-form.tsx
+++ components/forms/student-product-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 import * as z from 'zod';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { CalendarIcon, Trash } from 'lucide-react';
@@ -9,36 +9,24 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { format } from 'date-fns';
 import { cn } from '@/lib/utils';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Separator } from '@/components/ui/separator';
 import { Heading } from '@/components/ui/heading';
 import { useToast } from '../ui/use-toast';
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { Calendar } from '../ui/calendar';
 import { AlertModal } from '../modal/alert-modal';
+import { useDropzone } from 'react-dropzone';
+import Image from 'next/image';
 
-export const IMG_MAX_LIMIT = 3;
+export const IMG_MAX_LIMIT = 6;
 const formSchema = z.object({
-  firstName: z
-    .string()
-    .min(3, { message: 'First Name must be at least 3 characters' }),
-  middleName: z
-    .string()
-    .min(3, { message: 'Middle Name must be at least 3 characters' }),
-  lastName: z
-    .string()
-    .min(3, { message: 'Last Name must be at least 3 characters' }),
+  image: z.instanceof(File).refine((file) => file.size !== 0, 'Please upload an image'),
+  firstName: z.string().min(3, { message: 'First Name must be at least 3 characters' }),
+  middleName: z.string().min(3, { message: 'Middle Name must be at least 3 characters' }),
+  lastName: z.string().min(3, { message: 'Last Name must be at least 3 characters' }),
   birthDate: z.date(),
-  studentNo: z
-    .string()
-    .min(3, { message: 'Student Name must be at least 3 characters' }),
+  studentNo: z.string().min(3, { message: 'Student Name must be at least 3 characters' }),
   email: z.string().email({ message: 'Email Required' })
 });
 
@@ -49,22 +37,18 @@ interface ProductFormProps {
   categories: any;
 }
 
-export const StudentProductForm: React.FC<ProductFormProps> = ({
-  initialData,
-  categories
-}) => {
+export const StudentProductForm: React.FC<ProductFormProps> = ({ initialData, categories }) => {
   const [date, setDate] = useState<Date>();
   const params = useParams();
   const router = useRouter();
   const { toast } = useToast();
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [imgLoading, setImgLoading] = useState(false);
   const title = initialData ? 'Edit student' : 'Create student';
   const description = initialData ? 'Edit a student.' : 'Add a new student';
   const toastMessage = initialData ? 'Student updated.' : 'Student created.';
   const action = initialData ? 'Save changes' : 'Create';
-
+  const [preview, setPreview] = useState<any>();
   const defaultValues = initialData
     ? initialData
     : {
@@ -80,17 +64,37 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
     defaultValues
   });
 
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    const reader = new FileReader();
+    try {
+      reader.onload = () => setPreview(reader.result);
+      reader.readAsDataURL(acceptedFiles[0]);
+      form.setValue('image', acceptedFiles[0]);
+      form.clearErrors('image');
+    } catch (error) {
+      setPreview(null);
+      form.resetField('image');
+    }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    maxFiles: 1,
+    maxSize: 1000000,
+    accept: { 'image/png': [], 'image/jpg': [], 'image/jpeg': [] }
+  });
+
   const onSubmit = async (data: ProductFormValues) => {
     try {
       setLoading(true);
+      console.log(data);
       if (initialData) {
         // await axios.post(`/api/products/edit-product/${initialData._id}`, data);
       } else {
         // const res = await axios.post(`/api/products/create-product`, data);
         // console.log("product", res);
       }
-      router.refresh();
-      router.push(`/dashboard/products`);
+      // router.refresh();
+      // router.push(`/dashboard/student`);
       toast({
         variant: 'destructive',
         title: 'Uh oh! Something went wrong.',
@@ -122,31 +126,34 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
 
   return (
     <>
-      <AlertModal
-        isOpen={open}
-        onClose={() => setOpen(false)}
-        onConfirm={onDelete}
-        loading={loading}
-      />
+      <AlertModal isOpen={open} onClose={() => setOpen(false)} onConfirm={onDelete} loading={loading} />
       <div className="flex items-center justify-between">
         <Heading title={title} description={description} />
         {initialData && (
-          <Button
-            disabled={loading}
-            variant="destructive"
-            size="sm"
-            onClick={() => setOpen(true)}
-          >
+          <Button disabled={loading} variant="destructive" size="sm" onClick={() => setOpen(true)}>
             <Trash className="h-4 w-4" />
           </Button>
         )}
       </div>
       <Separator />
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-full space-y-8"
-        >
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
+          <FormField
+            control={form.control}
+            name="image"
+            render={() => (
+              <FormItem>
+                <FormLabel>Profile Picture</FormLabel>
+                <FormControl>
+                  <div {...getRootProps()} className="overflow-hidden">
+                    <Input {...getInputProps()} type="file" disabled={loading} />
+                    <Image src={preview || '/image-placeholder.png'} alt="Image" width={200} height={200} className=" rounded-md object-cover" />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="gap-8 md:grid md:grid-cols-3">
             <FormField
               control={form.control}
@@ -155,11 +162,7 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
                 <FormItem>
                   <FormLabel>Student No.</FormLabel>
                   <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="student no"
-                      {...field}
-                    />
+                    <Input disabled={loading} placeholder="Student no" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -172,11 +175,7 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
                 <FormItem>
                   <FormLabel>First Name</FormLabel>
                   <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="First name"
-                      {...field}
-                    />
+                    <Input disabled={loading} placeholder="First name" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -189,11 +188,7 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
                 <FormItem>
                   <FormLabel>Middle Name</FormLabel>
                   <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="Middle name"
-                      {...field}
-                    />
+                    <Input disabled={loading} placeholder="Middle name" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -207,11 +202,7 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
                 <FormItem>
                   <FormLabel>Last Name</FormLabel>
                   <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="Last name"
-                      {...field}
-                    />
+                    <Input disabled={loading} placeholder="Last name" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -225,11 +216,7 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
                 <FormItem>
                   <FormLabel>Email Address</FormLabel>
                   <FormControl>
-                    <Input
-                      disabled={loading}
-                      placeholder="Email Address"
-                      {...field}
-                    />
+                    <Input disabled={loading} placeholder="Email Address" {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -242,37 +229,22 @@ export const StudentProductForm: React.FC<ProductFormProps> = ({
                 <FormItem>
                   <FormLabel>Birth Date</FormLabel>
                   <FormControl>
-                    {/* <Input
-                      disabled={loading}
-                      placeholder="Email Address"
-                      {...field}
-                    /> */}
                     <Popover>
-                      <PopoverTrigger
-                        asChild
-                        disabled={loading}
-                        className=" flex"
-                      >
-                        <Button
-                          variant={'outline'}
-                          className={cn(
-                            'w-[240px] justify-start text-left font-normal',
-                            !date && 'text-muted-foreground'
-                          )}
-                        >
+                      <PopoverTrigger asChild disabled={loading} className=" flex">
+                        <Button variant={'outline'} className={cn(' w-full justify-start text-left font-normal', !date && 'text-muted-foreground')}>
                           <CalendarIcon className="mr-2 h-4 w-4" />
-                          {date ? (
-                            format(date, 'PPP')
-                          ) : (
-                            <span>Pick a date</span>
-                          )}
+                          {date ? format(date, 'PPP') : <span>Pick a date</span>}
                         </Button>
                       </PopoverTrigger>
                       <PopoverContent className="w-auto p-0" align="start">
                         <Calendar
+                          disabled={field.disabled}
                           mode="single"
                           selected={date}
-                          onSelect={setDate}
+                          onSelect={(e) => {
+                            field.onChange(e);
+                            setDate(e);
+                          }}
                           captionLayout="dropdown-buttons"
                           fromYear={1900}
                           toYear={2024}
---
+++ pages/api/student.ts
@@ -0,0 +1,115 @@
+import { auth } from '@/auth';
+import prisma from '@/lib/prisma';
+import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
+import cloudinary from '@/lib/cloudinary';
+import { Formidable, Fields, Files } from 'formidable';
+import { Students } from '@prisma/client';
+import nodemailer from 'nodemailer';
+import { hash } from 'bcryptjs';
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+interface StudentFieldFormidableType {
+  studentNo: Array<string>;
+  firstName: Array<string>;
+  middleName: Array<string>;
+  lastName: Array<string>;
+  birthDate: Array<Date>;
+  email: Array<string>;
+}
+
+interface StudentFileFormidableType {
+  images: Array<{
+    size: string;
+    newFilename: string;
+    filepath: string;
+    mimetype: string;
+    mtime: Date;
+    originalFilename: string;
+  }>;
+}
+
+export const parseForm = async (req: NextApiRequest): Promise<{ fields: StudentFieldFormidableType; files: StudentFileFormidableType | Files }> => {
+  return await new Promise<{
+    err: any;
+    fields: any;
+    files: StudentFileFormidableType | Files;
+  }>((resolve, reject) => {
+    const form = new Formidable();
+
+    return form.parse(req, (err, fields, files) => {
+      if (err) reject({ err });
+      resolve({ err, fields, files });
+    });
+  });
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const session = await auth(req, res);
+    if (session === null) return res.status(401).json({ message: 'Unauthorized', statusCode: 401 });
+    const userId = session.user && session.user.id;
+    if (!userId) return res.status(500).json({ message: 'Something went wrong', statusCode: 500 });
+
+    const { fields, files } = await parseForm(req);
+    if (!fields.studentNo || !fields.firstName || !fields.middleName || !fields.lastName || !fields.email || !fields.birthDate || !files.images)
+      return res.status(404).json({ message: 'All Field Required', statusCode: 404 });
+
+    const checkEmailExist = await prisma.students.findFirst({ where: { email: fields.email[0] } });
+    if (checkEmailExist) return res.status(409).json({ message: 'Email Already Exist', statusCode: 409 });
+
+    const generatePinCode = Math.random().toString().substr(2, 4);
+    console.log(generatePinCode);
+    const hashedPinCode = await hash(generatePinCode, 12);
+
+    // Start Send Node Email
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_APP_PASSWORD
+      }
+    });
+
+    const option = {
+      from: 'School Attendance <hello@gmail.com>',
+      to: fields.email[0],
+      subject: 'School Attendance OTP',
+      html: generatePinCode,
+      headers: {
+        priority: 'high',
+        importance: 'high'
+      }
+    };
+
+    const transEmail = await transporter.sendMail(option);
+    if (!transEmail.accepted[0]) {
+      return res.status(500).json({ message: 'Something went wrong', statusCode: 500 });
+    }
+    // End Send Node Email
+
+    // Start Upload Image into Cloudinary
+    const uploadImageCloud = await cloudinary.uploader.upload(files.images[0].filepath, {
+      folder: '/villanueva-school-atttendance'
+    });
+    if (!uploadImageCloud) return res.status(500).json({ message: 'Something went wrong', statusCode: 500 });
+    // End Upload Image into Cloudinary
+    const data = {
+      firstName: fields.firstName[0],
+      middleName: fields.middleName[0],
+      lastName: fields.lastName[0],
+      birthDate: new Date(fields.birthDate[0]),
+      studentNo: fields.studentNo[0],
+      email: fields.email[0],
+      pinCode: hashedPinCode,
+      images: uploadImageCloud.secure_url
+    };
+    await prisma.students.create({ data });
+    return res.json({ message: 'Student Created', statusCode: 200 });
+  }
+  return res.status(500).json({ message: 'Internal Server Error', status: 500 });
+}
