6d038a26829b58a0b8150fa0e567be31b57fc7f0
Kurt Russelle Marmol <jkurtrussellemarmol@gmail.com>
2024-07-14 23:04:01 +0800

    add suspense

--- app/(dashboard)/dashboard/student/page.tsx
+++ app/(dashboard)/dashboard/student/page.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from 'react';
 import BreadCrumb from '@/components/breadcrumb';
 import { StudentClient } from '@/components/tables/user-tables/student-client';
 import prisma from '@/lib/prisma';
+import StudentTableLoading from '@/app/(dashboard)/dashboard/student/student-table-loading';
 
 const breadcrumbItems = [{ title: 'Student', link: '/dashboard/student' }];
 export default async function Page() {
@@ -9,7 +11,9 @@ export default async function Page() {
     <>
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
         <BreadCrumb items={breadcrumbItems} />
-        <StudentClient data={data} />
+        <Suspense fallback={<p>Loading</p>}>
+          <StudentClient data={data} />
+        </Suspense>
       </div>
     </>
   );
---
+++ app/(dashboard)/dashboard/student/student-table-loading.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function StudentTableLoading() {
+  return <div>loading</div>;
+}
